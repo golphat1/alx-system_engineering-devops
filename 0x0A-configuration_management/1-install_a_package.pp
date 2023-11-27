0b6ff2c1add0046ae570e1ac0ba9e116449eab66
golphat1 <golphat@yahoo.com>
2023-11-27 09:39:05 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
-# Using Puppet to install flask from pip3
+# 1-install_a_package.pp
 
-package { 'puppet-lint':
-  ensure   => '4.2.3',
-  provider => 'gem',
+# Ensuring pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Installing Flask using pip3 with version 2.1.0
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep Version | grep 2.1.0',
+}
+
+# Notify when Flask is installed
+notify { 'Flask installed':
+  require => Exec['install_flask'],
 }
