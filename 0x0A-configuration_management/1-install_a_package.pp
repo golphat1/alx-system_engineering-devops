faeedc4044a5a4890c42e326f54ba00f809b3d21
golphat1 <golphat@yahoo.com>
2023-12-11 23:15:10 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
 #!/usr/bin/pup
 # install a package flask 2.1.0
-package {'flask':
-	ensure => '2.1.0',
-	provider => 'pip'
+
+package { 'python3-pip':
+  ensure => present,
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
